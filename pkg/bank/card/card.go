e6dd842b390a76316b3687ce132accf3b881531d
Sami <dima-5050@mail.ru>
2021-10-09 22:23:43 +0500

    реализована функсия PaymentSource

--- pkg/bank/card/card.go
+++ pkg/bank/card/card.go
@@ -19,3 +19,23 @@ func Total( cards []types.Card) types.Money  {
 	}
 	return sum
 }
+
+func PaymentSources(cards []types.Card) []types.PaymentSource  {
+	var paymentCardSlice []types.PaymentSource
+	var paymentCard types.PaymentSource
+	for _, card := range cards {
+		if !card.Active {
+			continue
+		}
+		
+		if card.Balance <= 0 {
+			continue
+		}
+
+		paymentCard.Number = string(card.PAN)
+		paymentCard.Balance = card.Balance
+		paymentCard.Type = card.Name
+		paymentCardSlice= append(paymentCardSlice, paymentCard)
+	}
+	return paymentCardSlice
+}
--- pkg/bank/card/example_test.go
+++ pkg/bank/card/example_test.go
@@ -42,3 +42,30 @@ func ExampleTotal() {
 
 	
 }
+
+func ExamplePaymentSource() {
+	payments := []types.Card {
+		{
+			PAN: "5058 xxxx xxxx 8888",
+			Balance: 1_000_00,
+			Active: true,	
+		},
+		{
+			PAN: "5058 xxxx xxxx 8887",
+			Balance: 1_000_00,
+			Active: false,
+		},
+		{
+			PAN: "5058 xxxx xxxx 8886",
+			Balance: -1_000_00,
+			Active: false,
+		},
+	}
+
+	num := PaymentSources(payments)
+	for _, pay := range num {
+		fmt.Println(pay.Number)		
+	}
+	
+	
+}
